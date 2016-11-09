901b68f196dcc64a29ce931badf34fb173c678c3
Rich Xu <richardxu100@gmail.com>
2016-11-09 12:00:00 -0500

    Add css to chat page

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
     console.log('createMessage', message);
     // socket.emit does to a single connection, io does for all connections
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server.');
+    callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
